72527a7a91dbdc77bfa9c28398aa0410dc9cef00
BorgesDev <borgesarthur.dev@gmail.com>
2024-04-16 11:22:30 -0300

    Learning React Basics - Creating components and displaying them.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { User } from "./User";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const planets = [
+    { name: "Mars",     isGasplanet: false },
+    { name: "Earth",    isGasplanet: false },
+    { name: "Jupiter",  isGasplanet: true },
+    { name: "Venus",    isGasplanet: false },
+    { name: "Neptune",  isGasplanet: true },
+    { name: "Uranus",   isGasplanet: true },
+  ]
+  return ( 
+  <div className="App">
+    {planets.map((planet,bool) => planet.isGasplanet && <h1> {planet.name} </h1>)}
+  </div>
   );
 }
 
+
+
 export default App;
+   
